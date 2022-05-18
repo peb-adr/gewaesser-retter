19a1a100f4faf9ba322dd4b23daf371cac1963c8
Jenny Krambach <jenny.krambach@intevation.de>
2022-05-18 09:17:57 +0200

    use german "datum" format (dd.mm.yyyy) in filters

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,6 +1,12 @@
 const today = new Date();
 const year = today.getFullYear();
 
+// translates german format "12.05.2022" (may 12) into correct Date
+const getDate = (input) => {
+  const a = input.split('.');
+  return new Date(`${a[1]}-${a[0]}-${a[2]}`);
+};
+
 const filters = [
   {
     label: "Alle",
@@ -8,37 +14,37 @@ const filters = [
   },
   {
     label: "Dieses Jahr",
-    fn: (i) => new Date(i.properties.date).getFullYear() === year,
+    fn: (i) => getDate(i.properties.datum).getFullYear() === year,
   },
   {
     label: (year - 1).toString(),
-    fn: (i) => new Date(i.properties.date).getFullYear() === year - 1,
+    fn: (i) => getDate(i.properties.datum).getFullYear() === year - 1,
   },
   {
     label: (year - 2).toString(),
-    fn: (i) => new Date(i.properties.date).getFullYear() === year - 2,
+    fn: (i) => getDate(i.properties.datum).getFullYear() === year - 2,
   },
   {
     label: (year - 3).toString(),
-    fn: (i) => new Date(i.properties.date).getFullYear() === year - 3,
+    fn: (i) => getDate(i.properties.datum).getFullYear() === year - 3,
   },
   {
     label: (year - 4).toString(),
-    fn: (i) => new Date(i.properties.date).getFullYear() === year - 4,
+    fn: (i) => getDate(i.properties.datum).getFullYear() === year - 4,
   },
   {
     label: `Ältere (vor ${(year - 4).toString()})`,
-    fn: (i) => new Date(i.properties.date).getFullYear() < year - 4,
+    fn: (i) => getDate(i.properties.datum).getFullYear() < year - 4,
   },
   {
     label: "Nächste zwei Wochen",
     fn: (i) =>
-      new Date(i.properties.date) < new Date().setDate(new Date() + 14) &&
-      new Date(i.properties.date) > new Date(),
+    getDate(i.properties.datum) < new Date().setDate(new Date() + 14) &&
+    getDate(i.properties.datum) > new Date(),
   },
   {
     label: "Zukünftig",
-    fn: (i) => new Date(i.properties.date) > new Date(),
+    fn: (i) => getDate(i.properties.datum) > new Date(),
   },
 ];
 export default filters;
